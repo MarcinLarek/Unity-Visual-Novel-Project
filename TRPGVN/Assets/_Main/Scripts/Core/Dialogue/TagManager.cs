a80029d2e51a1f45e3c2b5ba8c2aad953344cc84
MarcinLarek <94295943+MarcinLarek@users.noreply.github.com>
2024-09-01 17:33:51 +0200

    Operator Lines for Variables

--- TRPGVN/Assets/_Main/Scripts/Core/Dialogue/TagManager.cs
+++ TRPGVN/Assets/_Main/Scripts/Core/Dialogue/TagManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
 
@@ -15,18 +16,54 @@ public class TagManager
 
     private static readonly Regex tagRegex = new Regex("<\\w+>");
 
-    public static string Inject(string text)
+    public static string Inject(string text, bool injectTags = true, bool injectVariables = true)
     {
-        if (tagRegex.IsMatch(text))
+
+        if (injectTags)
+            text = InjectTags(text);
+
+        if (injectVariables)
+            text = InjectVariables(text);
+
+        return text;
+    }
+    private static string InjectTags(string value)
+    {
+        if (tagRegex.IsMatch(value))
         {
-            foreach(Match match in tagRegex.Matches(text))
+            foreach (Match match in tagRegex.Matches(value))
             {
-                if(tags.TryGetValue(match.Value, out var tagValueRequest))
+                if (tags.TryGetValue(match.Value, out var tagValueRequest))
                 {
-                    text = text.Replace(match.Value, tagValueRequest());
+                    value = value.Replace(match.Value, tagValueRequest());
                 }
             }
         }
-        return text;
+        return value;
+    }
+
+    private static string InjectVariables(string value)
+    {
+        var matches = Regex.Matches(value, VariableStore.REGEX_VARIABLE_IDS);
+        var matchesList = matches.Cast<Match>().ToList();
+
+        for(int i = matchesList.Count - 1; i>= 0; i--)
+        {
+            var match = matchesList[i];
+            string variableName = match.Value.TrimStart(VariableStore.VARIABLE_ID);
+
+            if(!VariableStore.TryGetValue(variableName, out object variableValue))
+            {
+                UnityEngine.Debug.LogError($"Variable {variableName} not found in streing assignment");
+                continue;
+            }
+
+            int lengthToBeRemoved = match.Index + match.Length > value.Length ? value.Length - match.Index : match.Length;
+
+            value = value.Remove(match.Index, lengthToBeRemoved);
+            value = value.Insert(match.Index, variableValue.ToString());
+        }
+
+        return value;
     }
 }
--- TRPGVN/Assets/_Main/Scripts/Core/Logical Lines/LogicalLineUtils.cs
+++ TRPGVN/Assets/_Main/Scripts/Core/Logical Lines/LogicalLineUtils.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -58,5 +59,142 @@ namespace DIALOGUE.LogicalLines
 
         }
 
+        public static class Expressions
+        {
+            public static HashSet<string> OPERATORS = new HashSet<string>() { "-", "-=", "+", "+=", "*", "*=", "/", "/=", "=" };
+            public static readonly string REGEX_ARITHMATIC = @"([-+*/=]=?)";
+            public static readonly string REGEX_OPERATOR_LINE = @"^\$\w+\s*(=|\+=|-=|\*=|/=|)\s*";
+
+            public static object CalculateValue(string[] expressionParts)
+            {
+                List<string> operandStrings = new List<string>();
+                List<string> operatorStrings = new List<string>();
+                List<object> operands = new List<object>();
+
+                for (int i = 0; i < expressionParts.Length; i++)
+                {
+                    string part = expressionParts[i].Trim();
+                    if (part == string.Empty)
+                        continue;
+
+                    if (OPERATORS.Contains(part))
+                        operatorStrings.Add(part);
+                    else
+                        operandStrings.Add(part);
+                }
+
+                foreach(string operandString in operandStrings)
+                {
+                    operands.Add(ExtractValue(operandString));
+                }
+
+                CalculateValue_DivisionAndMultiplication(operatorStrings, operands);
+                CalculateValue_AdditionAndSubstraction(operatorStrings, operands);
+
+                return operands[0];
+            }
+
+            private static void CalculateValue_DivisionAndMultiplication(List<string> operatorStrings, List<object> operands)
+            {
+                for (int i = 0; i < operatorStrings.Count; i++)
+                {
+                    string operatorString = operatorStrings[i];
+
+                    if (operatorString == "*" || operatorString == "/")
+                    {
+                        double leftOperand = Convert.ToDouble(operands[i]);
+                        double rightOperand = Convert.ToDouble(operands[i + 1]);
+
+                        if (operatorString == "*")
+                            operands[i] = leftOperand * rightOperand;
+                        else
+                        {
+                            if (rightOperand == 0)
+                            {
+                                Debug.LogError("Cannot divide by zero");
+                                return;
+                            }
+                            operands[i] = leftOperand / rightOperand;   
+                                
+                        }
+
+                        operands.RemoveAt(i+1);
+                        operatorStrings.RemoveAt(i);
+                        i--;
+                    }
+                }
+            }
+            private static void CalculateValue_AdditionAndSubstraction(List<string> operatorStrings, List<object> operands)
+            {
+                for (int i = 0; i < operatorStrings.Count; i++)
+                {
+                    string operatorString = operatorStrings[i];
+
+                    if(operatorString == "+" || operatorString == "-")
+                    {
+                        double leftOperand = Convert.ToDouble(operands[i]);
+                        double rightOperand = Convert.ToDouble(operands[i + 1]);
+
+                        if(operatorString == "+")
+                            operands[i] = leftOperand + rightOperand;
+                        else
+                            operands[i] = leftOperand - rightOperand;
+
+                        operands.RemoveAt(i+1);
+                        operatorStrings.RemoveAt(i);
+                        i--;
+                    }
+                }
+            }
+
+            private static object ExtractValue(string value)
+            {
+                bool negate = false;
+                if(value.StartsWith('!'))
+                {
+                    negate = true;
+                    value = value.Substring(1);
+                }
+
+                if (value.StartsWith(VariableStore.VARIABLE_ID))
+                {
+                    string variableName = value.TrimStart(VariableStore.VARIABLE_ID);
+                    if (!VariableStore.HasVariable(variableName))
+                    {
+                        Debug.LogError($"Variable {variableName} does no exist!");
+                        return null;
+                    }
+
+                    VariableStore.TryGetValue(variableName, out object val);
+
+                    if (val is bool boolVal && negate)
+                        return !boolVal;
+
+                    return val;
+                }
+                else if (value.StartsWith('\"') && value.EndsWith('\"'))
+                {
+                    value = TagManager.Inject(value, injectTags: true, injectVariables: true);
+                    return value.Trim('"');
+                }
+                else
+                {
+                    if (int.TryParse(value, out int intValue))
+                    {
+                        return intValue;
+                    }
+                    else if (float.TryParse(value, out float floatValue))
+                    {
+                        return floatValue;
+                    }
+                    else if (bool.TryParse(value, out bool boolValue))
+                    {
+                        return negate ? !boolValue : boolValue;
+                    }
+                    else
+                        return value;
+                }
+            }
+        }
     }
 }
